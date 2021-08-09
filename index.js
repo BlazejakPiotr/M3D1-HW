e75f8e8661d50c37b30671f96285a3a5e9836783
BlazejakPiotr <piotr.blazejak@yahoo.com>
2021-08-09 16:09:58 +0200

    ex3

--- index.js
+++ index.js
@@ -26,6 +26,10 @@ function isFifty(a, b) {
 Create a function to remove a character at the specified position of a given string and return the new string.
 */
 
+function removeChar(str, pos) {
+  return str.slice(0, pos) + str.slice(pos + 1);
+}
+
 /*
 
 4)
